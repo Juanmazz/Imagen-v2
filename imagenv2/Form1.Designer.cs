0c89c6e924a373cf43b31c450dd2c42117aed468
juanmazz <jaunmazziad@gmail.com>
2021-11-26 17:08:57 -0300

    Imagen v2

---
+++ imagenv2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace imagenv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(33, 12);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(430, 357);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(160, 392);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(163, 59);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Mostrar Imagen";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 463);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.pic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btn;
+    }
+}
+
---
+++ imagenv2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imagenv2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+
+            string[] imagenes=new string[5];
+
+            string dir= "C:\\Users\\JUAN PABLO\\Desktop\\Imagenes\\";
+
+            imagenes[0] = dir + "Cos1.jpg";
+            imagenes[1] = dir + "Cos2.jpg";
+            imagenes[2] = dir + "Cos3.jpg";
+            imagenes[3] = dir + "Cos4.jpg";
+            imagenes[4] = dir + "Cos5.jpg";
+
+            Random random = new Random();
+
+            pic.Image = Image.FromFile(imagenes[random.Next(5)]);
+
+        }
+    }
+}
